fedc2f5164b0ae3e0fb447f5bc001ebf6f8b2474
Philipp Auersperg-Castell <phil@bluedynamics.com>
2024-03-28 13:39:28 +0100

    adresse

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - One Page Wonder v6.0.6 (https://startbootstrap.com/theme/one-page-wonder)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
 */
 // This file is intentionally blank
